e2c49347620a24c13e79792eb9070ac165cabb0b
Martinpouh <martinchege2015@gmail.com>
2022-06-02 21:27:29 +0300

    commit tree ac14cbe3ae60a094ea93e4807c539ebf234215ac

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function introduction(name) {
 }
 
 function introductionWithLanguage(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return `Hi, my name is ${Martin} and I am learning to program in ${JavaScript}.`;
 }
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return `Hi, my name is ${Martin} and I am learning to program in ${JavaScript}.`;
 }
